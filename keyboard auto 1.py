686a996d9d0b5f73bf38fe5756fe96d0c34caea2
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-03-20 13:39:58 -0400

    Initial commit

---
+++ keyboard auto 1.py
@@ -0,0 +1,22 @@
+# documentation used: https://github.com/boppreh/keyboard#api
+import keyboard
+
+the_script = ['Test', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5',]
+i = 0
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
+i += 1
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
+i += 1
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
+i += 1
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
+i += 1
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
+i += 1
+keyboard.wait('ctrl+shift')
+keyboard.write(the_script[i], delay=0.03)
