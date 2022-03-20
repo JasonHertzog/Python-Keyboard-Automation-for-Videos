18ecee903b6f858a9171f594b975cd9763db7528
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-03-20 14:16:36 -0400

    Update keyboard auto 1.py
    More efficient

--- keyboard auto 1.py
+++ keyboard auto 1.py
@@ -1,22 +1,16 @@
 # documentation used: https://github.com/boppreh/keyboard#api
+from pynput.keyboard import Key, Controller
 import keyboard
 
-the_script = ['Test', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5',]
+# This is used to press enter with pynput
+enterboard = Controller() 
+
+the_script = ['Test', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5']
 i = 0
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
-i += 1
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
-i += 1
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
-i += 1
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
-i += 1
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
-i += 1
-keyboard.wait('ctrl+shift')
-keyboard.write(the_script[i], delay=0.03)
+keyboard.wait('shift')
+while i < len(the_script):
+    keyboard.write(the_script[i], delay=0.03)
+    enterboard.press(Key.enter)
+    enterboard.release(Key.enter)
+    i += 1
+    keyboard.wait('shift')
