d8f2131ca1071de44a3dcb51970518d474e864fb
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-03-23 07:53:12 -0400

    Added Teleprompter
    Added ability to see the next 8 lines of a script to enable more fluid live recording of the script. Commented code, and made small QoL improvements like assigning hotkey to a variable for easy changing later on.

--- keyboard auto 1.py
+++ keyboard auto 1.py
@@ -1,18 +1,42 @@
 # documentation used: https://github.com/boppreh/keyboard#api
 from pynput.keyboard import Key, Controller
 import keyboard
+# Importing os to be able to clear console and keep it clean when showing script.
+import os
 
 # This is used to press enter with pynput
 enterboard = Controller() 
+hotkey = 'shift'
 
-the_script = ['Test', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+the_script = ['Test', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5', 'Test6', 'Test7', 'Test8', 'Test9']
+# i is used for loop iterations.
 i = 0
-keyboard.wait('shift')
+# p is used to control the print iterations.
+p = 0
+
+# This will cause the program to wait until we start pressing the hotkey to begin.
+keyboard.wait(hotkey)
+
+# While loop that provides main functionality to the script. 
 while i < len(the_script):
+    # Types the text according to the script. Delay sets speed of each key.
     keyboard.write(the_script[i], delay=0.03)
+    # The next two lines will automatically press enter, which is helpful so we don't manually need to do that.
     enterboard.press(Key.enter)
     enterboard.release(Key.enter)
+    # Increase both iterations by 1 to match current script position.
     i += 1
-    keyboard.wait('shift')
+    p += 1
+    # Loop inside loop will make program less efficient, but since it isn't doing that much it should be fine.
+    os.system("cls")
+    # The != portion controls up to how many lines of text we want in the output box.
+    while (p < len(the_script)) & ((p - i) != 8):
+        print(the_script[p])
+        p += 1
+    # Reset p to i. This makes sure that the loop directly above this comment will run correctly when the next
+    # line of text is output.
+    p = i
+    # Wait for the hotkey again.
+    keyboard.wait(hotkey)
 
 # I want this to also print the next 5 lines of the script to the console so that I know what to say while going through.
